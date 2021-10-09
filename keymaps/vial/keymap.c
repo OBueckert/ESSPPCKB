2f0a1eb14be21bf1160615c3c709eeafd84c2bae
OBueckert <82766936+OBueckert@users.noreply.github.com>
2021-10-09 14:47:51 -0400

    Add files via upload

---
+++ keymaps/vial/keymap.c
@@ -0,0 +1,11 @@
+#include QMK_KEYBOARD_H
+
+#define ____ KC_TRNS
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(
+        KC_A, KC_A, KC_A, KC_A,
+        KC_A, KC_A, KC_A, KC_A
+    )
+};
